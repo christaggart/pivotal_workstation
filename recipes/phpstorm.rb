aadbd963195a1a39063f1ef641bf1775cd7c2c36
Chris Taggart <chris.taggart@canvaspop.com>
2013-04-15 16:57:11 -0400

    phpstorm

--- recipes/phpstorm.rb
+++ recipes/phpstorm.rb
@@ -4,5 +4,5 @@ dmg_package "PhpStorm" do
   source "http://download.jetbrains.com/webide/PhpStorm-5.0.4.dmg"
   checksum "825c03920212408b37da5c001517ae44037036d6a69be738eacf5d14e7cea80a"
   action :install
-  # owner WS_USER
+  owner WS_USER
 end
