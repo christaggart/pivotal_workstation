91cbd1b9456ddc5e345828cdf1d5053abfff5b46
Chris Taggart <chris.taggart@canvaspop.com>
2013-04-18 15:34:50 -0400

    update version of Vagrant

--- recipes/vagrant.rb
+++ recipes/vagrant.rb
@@ -1,7 +1,7 @@
 include_recipe "pivotal_workstation::virtualbox"
 
 dmg_package "Vagrant" do
-  source "http://files.vagrantup.com/packages/1e2d92d0ed73e28346bb74345c8e353bcab415fb/Vagrant.dmg"
+  source "http://files.vagrantup.com/packages/64e360814c3ad960d810456add977fd4c7d47ce6/Vagrant.dmg"
   checksum "a4d910b74c1e855ff601c31bfc56d0aa931035498426d92914d576f0b77df6b8"
   action :install
   type "pkg"
