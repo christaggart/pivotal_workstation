ca828206cc8cc51a8e0ecae24b9868dc7ecce177
Chris Taggart <chris.taggart@canvaspop.com>
2013-04-18 15:42:01 -0400

    updated to vagrant 1.2.1

--- recipes/vagrant.rb
+++ recipes/vagrant.rb
@@ -1,8 +1,8 @@
 include_recipe "pivotal_workstation::virtualbox"
 
 dmg_package "Vagrant" do
-  source "http://files.vagrantup.com/packages/64e360814c3ad960d810456add977fd4c7d47ce6/Vagrant.dmg"
-  checksum "a4d910b74c1e855ff601c31bfc56d0aa931035498426d92914d576f0b77df6b8"
+  source "http://files.vagrantup.com/packages/a7853fe7b7f08dbedbc934eb9230d33be6bf746f/Vagrant-1.2.1.dmg"
+  checksum "27fe439c2e0521751e96dc2a45979a85272b5fc528ba012a0c9c0d527e7ac117"
   action :install
   type "pkg"
   owner WS_USER
