1be2010d2cab27bf9945733a41854600533da3c4
Chris Taggart <chris.taggart@canvaspop.com>
2013-04-26 15:28:48 -0400

    comment out custom github keys for now

--- recipes/github_ssh_keys.rb
+++ recipes/github_ssh_keys.rb
@@ -30,27 +30,27 @@ execute "make sure .ssh/config is owned by the user" do
   only_if "test -e #{WS_HOME}/.ssh/config"
 end
 
-execute "create SSH key pair for Github" do
-  command "ssh-keygen -N '' -f #{WS_HOME}/.ssh/id_github_#{node["github_project"] || node['fqdn']}"
-  user WS_USER
-  not_if "test -e #{WS_HOME}/.ssh/id_github_#{node["github_project"] || node['fqdn']}"
-end
+#execute "create SSH key pair for Github" do
+#  command "ssh-keygen -N '' -f #{WS_HOME}/.ssh/id_github_#{node["github_project"] || node['fqdn']}"
+#  user WS_USER
+#  not_if "test -e #{WS_HOME}/.ssh/id_github_#{node["github_project"] || node['fqdn']}"
+#end
 
-execute "symlink Github key for git-project" do
-  command "ln -nfs #{WS_HOME}/.ssh/id_github_{#{node["github_project"] || node['fqdn']},current}"
-  user WS_USER
-  only_if { node.has_key?("github_project") }
-end
+#execute "symlink Github key for git-project" do
+#  command "ln -nfs #{WS_HOME}/.ssh/id_github_{#{node["github_project"] || node['fqdn']},current}"
+#  user WS_USER
+#  only_if { node.has_key?("github_project") }
+#end
 
-execute "symlink Github public key for git-project" do
-  command "ln -nfs #{WS_HOME}/.ssh/id_github_{#{node["github_project"] || node['fqdn']},current}.pub"
-  user WS_USER
-  only_if { node.has_key?("github_project") }
-end
+#execute "symlink Github public key for git-project" do
+#  command "ln -nfs #{WS_HOME}/.ssh/id_github_{#{node["github_project"] || node['fqdn']},current}.pub"
+#  user WS_USER
+#  only_if { node.has_key?("github_project") }
+#end
 
-execute "add Github configuration to .ssh/config" do
-  config = "\n\nHost github.com\n  User git\n  IdentityFile #{WS_HOME}/.ssh/id_github_current"
-  command "echo '#{config}' >> #{WS_HOME}/.ssh/config"
-  not_if "grep 'id_github_current' #{WS_HOME}/.ssh/config"
-  user WS_USER
-end
+#execute "add Github configuration to .ssh/config" do
+#  config = "\n\nHost github.com\n  User git\n  IdentityFile #{WS_HOME}/.ssh/id_github_current"
+#  command "echo '#{config}' >> #{WS_HOME}/.ssh/config"
+#  not_if "grep 'id_github_current' #{WS_HOME}/.ssh/config"
+#  user WS_USER
+#end
